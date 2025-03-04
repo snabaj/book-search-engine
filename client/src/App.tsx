6dea0b231ae7dc7c6a6828abafe02e9839f27599
Star <snabajja@gmail.com>
2025-03-04 14:42:52 -0500

    Refactor App.tsx to use Apollo Provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,42 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Navbar from './components/Navbar';
+
+// Create a new ApolloClient instance
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', 
+  cache: new InMemoryCache(),           
+});
 
 function App() {
   return (
-    <>
+    // Wrap the entire app in the ApolloProvider so all components can use Apollo
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+// import './App.css';
+// import { Outlet } from 'react-router-dom';
+
+// import Navbar from './components/Navbar';
+
+// function App() {
+//   return (
+//     <>
+//       <Navbar />
+//       <Outlet />
+//     </>
+//   );
+// }
+
+// export default App;
