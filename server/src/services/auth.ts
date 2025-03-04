3190a52c8b4d5d17f9fae8f1267c404143bdbe31
Star <snabajja@gmail.com>
2025-03-03 19:03:48 -0500

    update auth middleware to work with the GraphQL API

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
+import { GraphQLError } from 'graphql';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -13,23 +14,30 @@ interface JwtPayload {
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
-  if (authHeader) {
+  if (!authHeader) {
+    throw new GraphQLError('Authorization token is requried', {
+      extensions: {
+        code: 'UNAUTHENTICATED',
+      },
+    });
+  }
+
     const token = authHeader.split(' ')[1];
 
     const secretKey = process.env.JWT_SECRET_KEY || '';
 
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
+    try {
+    const decoded = jwt.verify(token, secretKey) as JwtPayload;
+    (req as any).user = decoded;
+     next();
+  } catch (error) {
+    throw new GraphQLError('Invalid token', {
+      extensions: {
+        code: 'UNAUTHENTICATED',
+      },
     });
-  } else {
-    res.sendStatus(401); // Unauthorized
   }
-};
+}
 
 export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
