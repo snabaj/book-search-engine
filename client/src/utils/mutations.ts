6f6bea2c3fb74a511b036f5daa7c5ae20ee43b07
Star <snabajja@gmail.com>
2025-03-04 14:34:20 -0500

    Refactor mutations.ts to use GraphQL mutations for user login, user creation, saving a book, and removing a book

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,88 @@
+import { gql } from '@apollo/client';
+
+// LOGIN_USER mutation: Logs a user in using their email and password.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+// ADD_USER mutation: Creates a new user with a username, email, and password.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+// SAVE_BOOK mutation: Saves a book to the user's savedBooks list.
+// Uses a single 'bookData' input to keep things tidy (like a one-stop-shop for book details).
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// REMOVE_BOOK mutation: Removes a book from the user's savedBooks based on the bookId.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
