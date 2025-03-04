0bdc44bca0bea1cdaffb75c544d2f7f2ae95e082
Star <snabajja@gmail.com>
2025-03-04 11:18:09 -0500

    Refactor server.ts to use Apollo Server for GraphQL API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,60 +1,60 @@
-// import express from 'express';
-// //import path from 'node:path';
-// import db from './config/connection.js';
-// //import routes from './routes/index.js';
-// import { ApolloServer } from '@apollo/server';
-// import { expressMiddleware } from '@apollo/server/express4';
-// import { typeDefs, resolvers } from './schemas/index.js';
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
-
-// const startApolloServer = async () => {
-//   await server.start();
-//   await db();
-
-//   const PORT = process.env.PORT || 3001;
-//   const app = express();
-
-//   app.use(express.urlencoded({ extended: false }));
-//   app.use(express.json());
-
-//   app.use('/graphql', expressMiddleware(server));
-
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-//   });
-// };
-
-// startApolloServer();
-
-
-
-
-
-
-
 import express from 'express';
-import path from 'node:path';
+//import path from 'node:path';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+//import routes from './routes/index.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server));
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
+
+startApolloServer();
+
+
+
+
+
+
+
+// import express from 'express';
+// import path from 'node:path';
+// import db from './config/connection.js';
+// import routes from './routes/index.js';
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
