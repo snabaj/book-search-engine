8f4cb0054ec830e374cb5f0cec9e2cd394bbf372
Star <snabajja@gmail.com>
2025-03-09 14:24:57 -0400

    Move API search function to top of page

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,6 +1,11 @@
+//make a search to google books api
+//https://www.googleapis.com/books/v1/volumes?q=harry+potter
+
 export const searchGoogleBooks = (query: string) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
+
+
 // import type { User } from '../models/User.js';
 // import type { Book } from '../models/Book.js';
 
@@ -56,5 +61,3 @@ export const searchGoogleBooks = (query: string) => {
 //   });
 // };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
