12ddbef715f511c66580df12973712c0048c3414
Star <snabajja@gmail.com>
2025-03-10 23:50:04 -0400

    Refactor server.ts to include authentication middleware and serve static files in production

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
-//import path from 'node:path';
-import db from './config/connection.js';
-//import routes from './routes/index.js';
+import path from 'node:path';
+import type { Request, Response } from 'express';
+import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './services/auth.js';
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 const startApolloServer = async () => {
@@ -21,7 +22,19 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server));
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
@@ -30,6 +43,7 @@ const startApolloServer = async () => {
 };
 
 startApolloServer();
+
 
 
 
